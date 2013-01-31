6fba2ab61540f4f422c5eec5a6b7a0f58e444e35
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2013-01-31 19:58:29 +0100

    Migrate to socket.io server.

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from socketio.server import SocketIOServer
+from werkzeug.wsgi import SharedDataMiddleware
 from gallery.views import gallery
 
 app = Flask(__name__)
@@ -6,4 +8,13 @@ app.register_blueprint(gallery, url_prefix='/gallery')
 
 
 if __name__ == '__main__':
+    app.debug = True
+    app.logger.info('Listening on http://localhost:8080')
+    server = SocketIOServer(
+        ('0.0.0.0', 8080),
+        SharedDataMiddleware(app, {}),
+        namespace="socket.io",
+        policy_server=False
+    )
+    server.serve_forever()
     app.run(debug=True)
